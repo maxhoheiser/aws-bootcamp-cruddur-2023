9310e0f993ac764017a04711b2d3ad1c9b87691a
Maximilian Hoheiser <max.hoheiser@gmail.com>
2024-02-19 23:42:53 +0100

    feat: add opentelementry

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,6 +2,12 @@ import os
 
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from services.create_activity import CreateActivity
 from services.create_message import CreateMessage
 from services.create_reply import CreateReply
@@ -13,7 +19,18 @@ from services.search_activities import SearchActivities
 from services.show_activity import ShowActivities
 from services.user_activities import UserActivities
 
+# Initialize tracing and an exporter that can send data to Honeycomb
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
+# Initialize automatic instrumentation with Flask
 app = Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
 frontend = os.getenv("FRONTEND_URL")
 backend = os.getenv("BACKEND_URL")
 origins = [frontend, backend]
